8696b352798475980a391d76b648d6b1c626c60b
Bimbo Escodero Villaganas <villaganasbimbo143@gmail.com>
2024-06-04 03:20:07 +0800

    Updates

--- assets/ios.transition-legacy-NaS_LTvR.js
+++ assets/ios.transition-legacy-NaS_LTvR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6uo_2jIO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BCt1HYkk.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-bnTwtjZp.js
+++ assets/md.transition-legacy-bnTwtjZp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6uo_2jIO.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-BCt1HYkk.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
