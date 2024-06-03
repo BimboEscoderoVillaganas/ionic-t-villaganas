f95a8bae7ffabc5e1a5fb91ad8e529f95227e404
Bimbo Escodero Villaganas <villaganasbimbo143@gmail.com>
2024-06-04 03:36:09 +0800

    Updates

--- assets/md.transition-legacy-DbsUsgWF.js
+++ assets/md.transition-legacy-DbsUsgWF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BCt1HYkk.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DI7Y2zrN.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
