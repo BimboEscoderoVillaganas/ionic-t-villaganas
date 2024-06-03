bf927bce4934ec8305d90ba5419815a99cbec3a2
Bimbo Escodero Villaganas <villaganasbimbo143@gmail.com>
2024-06-04 04:09:26 +0800

    Updates

--- assets/swipe-back-legacy-BDkUDHOn.js
+++ assets/swipe-back-legacy-BDkUDHOn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DI7Y2zrN.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CSehug2H.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
